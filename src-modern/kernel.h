c0cbfecee18954fade9226e8936cca30b7902e20
Jon Brennecke <jpbrennecke@gmail.com>
2014-09-30 22:23:31 -0700

    Added the new dev-experimental branch, which is the first beginnings of the new, modernized AppWare kernel and runtime engine.

---
+++ src-modern/kernel.h
@@ -0,0 +1,51 @@
+
+/*
+//////////////////////////////////////////////////////////////////////////////////////////
+
+                   __          __            
+     /\            \ \        / /            
+    /  \   _ __  _ _\ \  /\  / /_ _ _ __ ___ 
+   / /\ \ | '_ \| '_ \ \/  \/ / _` | '__/ _ \
+  / ____ \| |_) | |_) \  /\  / (_| | | |  __/
+ /_/    \_\ .__/| .__/ \/  \/ \__,_|_|  \___|
+          | |   | |                          
+          |_|   |_|    
+
+
+The kernel is written in C, since it will need to be used from both C++ and Object-C.
+
+//////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+#ifndef __AW_KERNEL__
+#define __AW_KERNEL__
+
+/*
+
+	AppWare essential definitions
+
+*/
+
+#define __AW__
+#define __AW_VERSION__ 0.0.1
+
+
+/* 
+
+	All appware source is contained within the aw namespace.
+
+*/
+namespace aw
+{
+
+
+	namespace kernel
+	{
+		
+	}
+
+
+}
+
+
+#endif
